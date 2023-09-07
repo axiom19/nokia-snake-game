565e07dfff7136f0d538f18ddd20f93dd08c4e69
Shagundeep Singh <99030373+axiom19@users.noreply.github.com>
2023-09-07 16:43:10 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title("Nokia Sapp")
+
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+score_counter = 0
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detection of collision with food
+    if snake.head.distance(food) < 13:
+        food.refresh()
+        score_counter += 1
+        scoreboard.update_score(score_counter)
+        snake.extend()
+
+    # detect collision with wall
+    if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
+        scoreboard.game_over()
+        game_is_on = False
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
